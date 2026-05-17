90b1906c163804ba02db3383281f04b218c774d1
WikiCoding <tiagomscastro@gmail.com>
2026-05-17 17:51:54 +0100

    init commit

--- src/main/java/com/wikicoding/grpc/spring_grpc_demo/client/HelloController.java
+++ src/main/java/com/wikicoding/grpc/spring_grpc_demo/client/HelloController.java
@@ -1,5 +1,6 @@
 package com.wikicoding.grpc.spring_grpc_demo.client;
 
+import io.grpc.StatusRuntimeException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,14 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<Object> getHello(@RequestParam(value = "name") String name) {
-        return ResponseEntity.ok(grpcClientService.getHello(name));
+        try {
+            String response = grpcClientService.getHello(name);
+
+            return ResponseEntity.ok(response);
+        } catch (StatusRuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 }
